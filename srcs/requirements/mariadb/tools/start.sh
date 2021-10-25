02938362046a839c658bcb7924b7889ea42cbc91
driwand <44057801+driwand@users.noreply.github.com>
2021-10-25 19:25:12 +0100

    add wordpress query

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -2,7 +2,6 @@ if [ ! -f "/var/lib/mysql/ibdata1" ]; then
 	mysql_install_db
 fi
 
-
 service mysql start
 
 sed -i s/'127.0.0.1'/'0.0.0.0'/g /etc/mysql/mariadb.conf.d/50-server.cnf
@@ -16,6 +15,8 @@ mysql -e "FLUSH PRIVILEGES;"
 mysql -e "CREATE USER '${DB_USER}'@'%' identified by '${DB_PASS}';"
 mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}'"
 mysql -e "FLUSH PRIVILEGES;"
+
+mysql -e < /wp.sql
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';"
 mysql -uroot -ptoor -e "FLUSH PRIVILEGES;"
