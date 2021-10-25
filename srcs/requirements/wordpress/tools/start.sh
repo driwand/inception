d3618a9342d8955b409bb86cfe39ad7b6d16309d
driwand <44057801+driwand@users.noreply.github.com>
2021-10-25 15:17:47 +0100

    add wp download to dockerfile

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,6 +1,3 @@
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
-chmod +x /usr/local/bin/wp
-wp core download --allow-root
 
 if [ ! -f "/www/wp-config.php" ]; then
 	wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost=$DB_HOST --path="/www" --dbprefix=wp_ --allow-root
