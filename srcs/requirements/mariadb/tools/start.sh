eb9e8a997237c3acb7759cca950d9ba0b3d92baf
driwand <44057801+driwand@users.noreply.github.com>
2021-10-25 15:17:28 +0100

    changed root password

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -7,11 +7,19 @@ service mysql start
 
 sed -i s/'127.0.0.1'/'0.0.0.0'/g /etc/mysql/mariadb.conf.d/50-server.cnf
 
-mysql -uroot -e "CREATE DATABASE ${DB_NAME};"
-mysql -uroot -e "CREATE USER '${DB_NAME}'@'%' identified by '${DB_PASS}';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_NAME}'@'%' IDENTIFIED BY '${DB_PASS}'"
-mysql -uroot -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE ${DB_NAME};"
 
-service mysql stop
+mysql -e "CREATE USER '${DB_USER}'@'localhost' identified by '${DB_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}'"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysql -e "CREATE USER '${DB_USER}'@'%' identified by '${DB_PASS}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}'"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';"
+mysql -uroot -ptoor -e "FLUSH PRIVILEGES;"
+
+mysqladmin -uroot -p${ROOT_PASS} shutdown
 
 mysqld_safe
