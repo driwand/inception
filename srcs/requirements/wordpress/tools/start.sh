011f41399dafc03b2c4804cc6fd29a3ab44f4e0e
driwand <44057801+driwand@users.noreply.github.com>
2021-10-29 17:35:36 +0100

    update

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,10 +1,11 @@
-sed -i "s/DB_USERNAME/$DB_USER/g" /www/wp-config.php
-sed -i "s/DB_USERPASS/$DB_PASS/g" /www/wp-config.php
 
 if [ ! -f "/www/wp-config.php" ]; then
 	cp /wp-config.php /www
 	cp -r ~/wordpress/* /www
 	cp -r ~/redis-cache /www/wp-content/plugins
 	cp /www/wp-content/plugins/redis-cache/includes/object-cache.php /www/wp-content
+
+	sed -i "s/DB_USERNAME/$DB_USER/g" /www/wp-config.php
+	sed -i "s/DB_USERPASS/$DB_PASS/g" /www/wp-config.php
 fi
 
